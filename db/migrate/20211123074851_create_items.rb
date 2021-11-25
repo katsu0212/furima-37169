10a86f5c826ad5fbaa47e08b6cfac783bd9d14c0
katsu0212 <katsu02122728@gmail.com>
2021-11-25 10:49:41 +0900

    deviseのインストール

--- db/migrate/20211123074851_create_items.rb
+++ db/migrate/20211123074851_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name, null: false
+      t.text :introduction, null: false
+      t.integer :category_id, null: false
+      t.integer :item_condition_id, null: false
+      t.integer :postage_id, null: false
+      t.integer :region_id, null: false
+      t.integer :preparation_day_id, null: false
+      t.integer :price, null: false
+      t.referrences :user, null: false, foreign_key: true
       t.timestamps
     end
   end
