848dbaa813816441d7d20f616106db9cd09e98ad
katsu0212 <katsu02122728@gmail.com>
2021-11-25 23:24:06 +0900

    テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,14 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchases
 
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
-  validates :nickname,        presence: true
-  validates :birthday,        presence: true
-  
+  with_options presence: true do
+    validates :last_name,       format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :first_name,      format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :last_name_kana,  format: {with: /\A[ァ-ヶー－]+\z/}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
+    validates :password,:password_confirmation, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+    validates :nickname       
+    validates :birthday        
+  end
+    
 end
