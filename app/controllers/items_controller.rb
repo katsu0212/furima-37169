dbd25600bfb685c86c3a88b7926ab6351beba5e0
katsu0212 <katsu02122728@gmail.com>
2021-11-26 16:29:30 +0900

    itemモデル作り直し

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,15 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+  end
+  
+  def create
+    @item = Item.find(item_params)
+    @item.save
+  end
+
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :introduction, :category_id, :item_condition_id, :postage_id, :region_id, :preparation_day_id, :price)
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,12 +3,14 @@ class Item < ApplicationRecord
   has_one :purchase
   has_one_attached :image
 
-  validates :name,                presence: true 
-  validates :introduction,        presence: true
-  validates :category_id,         presence: true
-  validates :item_condition_id,   presence: true
-  validates :postage_id,          presence: true
-  validates :region_id,           presence: true 
-  validates :preparation_day_id,  presence: true
-  validates :price,               presence: true
+  with_options presence: true do
+    validates :name          
+    validates :introduction        
+    validates :category_id    
+    validates :item_condition_id   
+    validates :postage_id          
+    validates :region_id    
+    validates :preparation_day_id
+    validates :price,               format: {with: /\A[0-9]+\z/} 
+  end
 end
