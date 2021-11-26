5539afb675010c83532b4db24ea04b0f7e639120
katsu0212 <katsu02122728@gmail.com>
2021-11-26 13:30:05 +0900

    itemテーブルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
-  
+  belongs_to :user
+  has_one :purchase
+
+  validates :name,                presence: true 
+  validates :introduction,        presence: true
+  validates :category_id,         presence: true
+  validates :item_condition_id,   presence: true
+  validates :postage_id,          presence: true
+  validates :region_id,           presence: true 
+  validates :preparation_day_id,  presence: true
+  validates :price,               presence: true
 end
