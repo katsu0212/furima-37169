806874920dc2b7488585a06cf2b0cb8a7a52e8d0
katsu0212 <katsu02122728@gmail.com>
2021-11-26 11:22:41 +0900

    テストコード・バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ApplicationRecord
   has_many :purchases
 
   with_options presence: true do
-    validates :last_name,       format: {with: /\A[ぁ-んァ-ン一-龥]/}
-    validates :first_name,      format: {with: /\A[ぁ-んァ-ン一-龥]/}
-    validates :last_name_kana,  format: {with: /\A[ァ-ヶー－]+\z/}
-    validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
-    validates :password,:password_confirmation, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+    validates :last_name,                       format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+    validates :first_name,                      format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+    validates :last_name_kana,                  format: {with: /\A[ァ-ヶー－]+\z/}
+    validates :first_name_kana,                 format: {with: /\A[ァ-ヶー－]+\z/}
+    validates :password,:password_confirmation, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
     validates :nickname       
     validates :birthday        
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -53,10 +53,23 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
 
-      it 'passwordは英数字混合でないと登録できない' do
+      it 'passwordは英字のみでは登録できない' do
         @user.password = 'aaaaaa'
         @user.password_confirmation ='aaaaaa'
         @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end 
+      it 'passwordは数字のみでは登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation ='123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end 
+      it '全角文字を含むpasswordは登録できない' do
+        @user.password = 'アイウエオカキ'
+        @user.password_confirmation ='アイウエオカキ'
+        @user.valid?
+        binding.pry
         expect(@user.errors.full_messages).to include("Password is invalid")
       end 
 
