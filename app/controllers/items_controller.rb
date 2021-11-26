8240e152b0d851c2ad8999b9571da91ce3a0ec12
katsu0212 <katsu02122728@gmail.com>
2021-11-26 15:05:13 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,9 @@
 class ItemsController < ApplicationController
   def index
-   
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :category_id, :item_condition_id, :postage_id, :region_id, :preparation_day_id, :price)
   end
 end
