4b733f48eafa6cd0f240339e533a54886bd174cf
katsu0212 <katsu02122728@gmail.com>
2021-11-27 17:03:33 +0900

    テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,10 +15,10 @@ class Item < ApplicationRecord
     validates :image          
     validates :introduction        
     validates :category_id,         numericality: { other_than: 1 , message: "can't be blank"}
-    validates :item_condition_id   
-    validates :postage_id          
-    validates :region_id    
-    validates :preparation_day_id
+    validates :item_condition_id,   numericality: { other_than: 1 , message: "can't be blank"}
+    validates :postage_id,          numericality: { other_than: 1 , message: "can't be blank"}
+    validates :region_id,           numericality: { other_than: 1 , message: "can't be blank"}
+    validates :preparation_day_id,  numericality: { other_than: 1 , message: "can't be blank"}
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },              format: { with: /\A[0-9]+\z/ } 
   end
 end
