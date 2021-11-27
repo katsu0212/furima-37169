bd5950bd5a61ba0535c72b72c8c58693bff32212
katsu0212 <katsu02122728@gmail.com>
2021-11-27 10:56:07 +0900

    エラーメッセージ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.all
   end
 
   def new
@@ -7,8 +8,12 @@ class ItemsController < ApplicationController
   end
   
   def create
-    @item = Item.find(item_params)
-    @item.save
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
