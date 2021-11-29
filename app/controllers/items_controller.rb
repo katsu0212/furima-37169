e286ba35f2a5748bc492884fcf4a9508c38778ff
katsu0212 <katsu02122728@gmail.com>
2021-11-29 11:34:57 +0900

    消去機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :edit, :update]
+  before_action :authenticate_user!, only:[:new, :edit, :update, :destroy]
   before_action :set_item, only:[:show, :edit,:update]
   def index
     @items = Item.all.order(created_at: "DESC")
@@ -36,8 +36,13 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
   def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
   end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :introduction, :category_id, :item_condition_id, :postage_id, :region_id, :preparation_day_id, :price).merge(user_id: current_user.id)
